32eeb4fc9de7527ba7ea72ef453c999811485b76
shabil15 <mohammedshabil15@gmail.com>
2024-09-12 00:55:03 -0700

    feat(backend): refactor index and add CreditCard routes for credit cards dashboard

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import sequelize from './config/db.config';
+import crediCardRoutes from './routes/creditCards';
 
 const app = express();
 app.use(express.json());
+
+app.use('/api/credit-cards',crediCardRoutes);
 
 app.get('/',(req,res)=>{
     res.send('Credit cards Dashboard backend');
@@ -13,8 +16,8 @@ const PORT = process.env.PORT || 9000;
 sequelize.sync()
   .then(() => {
     console.log('Database connected and synced');
-    app.listen(5000, () => {
-      console.log('Server is running on port 5000');
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}` );
     });
   })
   .catch((err) => console.log('Error: ', err));
