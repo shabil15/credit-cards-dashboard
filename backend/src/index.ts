c4615b5d4a535119a35fd88caae9965fdccedeeb
shabil15 <mohammedshabil15@gmail.com>
2024-09-11 23:13:16 -0700

    (feat(backend): refactor index and add db sync for credit cards dashboard)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import sequelize from './config/db.config';
 
 const app = express();
 app.use(express.json());
@@ -9,4 +10,11 @@ app.get('/',(req,res)=>{
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT,()=> console.log(`Server running on port ${PORT}`));
+sequelize.sync()
+  .then(() => {
+    console.log('Database connected and synced');
+    app.listen(5000, () => {
+      console.log('Server is running on port 5000');
+    });
+  })
+  .catch((err) => console.log('Error: ', err));
