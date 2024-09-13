c121a2136209892297102d314a8992d5b13183dc
shabil15 <mohammedshabil15@gmail.com>
2024-09-13 03:20:29 -0700

    feat(frontend): create Banks component for new banks page

---
+++ frontend/src/components/banks.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banks: React.FC = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-3xl font-bold">This is Banks Page</h1>
+    </div>
+  );
+};
+
+export default Banks;
