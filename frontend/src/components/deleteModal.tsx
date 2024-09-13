5b410855ab7f66c09b33c33c0b989fc924e15023
shabil15 <mohammedshabil15@gmail.com>
2024-09-13 03:18:43 -0700

    feat(frontend): refactor DeleteModal component with responsive UI and success message for succesful deletion

--- frontend/src/components/deleteModal.tsx
+++ frontend/src/components/deleteModal.tsx
@@ -1,36 +1,48 @@
 import React from 'react';
 import { CreditCard } from '../types/creditCard';
 import { deleteCreditCard } from '../services/creditCardService';
+import { toast } from "react-toastify";
 
 interface DeleteModalProps {
-    card:CreditCard | null;
-    onClose: () => void;
-    onDelete: () => void;
+  card: CreditCard | null;
+  onClose: () => void;
+  onDelete: () => void;
 }
 
 const DeleteModal: React.FC<DeleteModalProps> = ({ card, onClose, onDelete }) => {
+  const handleDelete = async () => {
+    if (card) {
+      await deleteCreditCard(card.id);
+      toast.success("Credit card deleted successfully!");
+      onDelete();
+      onClose();
+    }
+  };
 
-    const handleDelete = async () => {
-        if(card){
-        await deleteCreditCard(card.id);
-        onDelete();
-        onClose();
-        }
-    };
-
-    return (
-        <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
-          <div className="bg-white rounded-lg p-6 w-96">
-            <h2 className='text-xl mb-4'>Confirm Deletion </h2>
-            <p className='mb-4'>Are you sure ?</p>
-            <div className="flex justify-between">
-          <button className="bg-gray-500 text-white px-4 py-2 rounded" onClick={onClose}>Cancel</button>
-          <button className="bg-red-500 text-white px-4 py-2 rounded" onClick={handleDelete}>Delete</button>
+  return (
+    <div className="fixed inset-0 bg-gray-800 bg-opacity-75 flex items-center justify-center">
+      <div className="relative p-6 w-80 rounded-3xl shadow-lg backdrop-blur-lg bg-white bg-opacity-20 border border-white border-opacity-30">
+        <h2 className="text-xl font-bold mb-4 text-center text-white">Confirm Deletion</h2>
+        <p className="text-center mb-6 text-white">Are you sure?</p>
+        <hr className="border-gray-400 border-opacity-30 mb-4" />
+        <div className="flex">
+          <button
+            onClick={handleDelete}
+            className="flex-1 bg-red-500 bg-opacity-70 text-white py-2 rounded-bl-xl hover:bg-opacity-90 transition duration-200"
+          >
+            Yes
+          </button>
+          <div className="w-px bg-white bg-opacity-30"></div>
+          <button
+            onClick={onClose}
+            className="flex-1 bg-gray-300 bg-opacity-50 text-white py-2 rounded-br-xl hover:bg-opacity-70 transition duration-200"
+          >
+            No
+          </button>
         </div>
-          </div>
-        </div>
-        
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default DeleteModal;
